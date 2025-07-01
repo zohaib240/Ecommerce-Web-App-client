2c47ce10316664beac065f7bac8deacfbbffb921
zohaib240 <zohaibjs123@gmail.com>
2025-07-01 08:21:08 +0500

    register.jsx

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,7 +1,7 @@
 import  { useRef, useState } from 'react';
 import axios from 'axios';
 import Swal from 'sweetalert2';
-import { useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
 const Register = () => {
   const fullName = useRef();
